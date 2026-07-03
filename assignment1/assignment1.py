8eac73fc40e2c26fee873192222c7cb68c1d14b1
belunatic <abelluvyou@yahoo.com>
2026-07-03 12:37:25 -0700

    task 3 done

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -5,3 +5,39 @@ def hello():
 # task2
 def greet(name):
     return "Hello, " + name + "!"
+
+# task3
+def calc(num1, num2, operation="multiply"):
+    if operation == "multiply":
+        try:
+            return num1 * num2
+        except TypeError:
+            return "You can't multiply those values!"
+    elif operation == "add":
+        try:
+            return num1 + num2
+        except TypeError:
+            return "You can't add those values!"
+    elif operation == "subtract":
+        try:
+            return num1 - num2
+        except TypeError:
+            return "You can't subtract those values!"
+    elif operation == "divide":
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            return "You can't divide by 0!"
+        except TypeError:
+            return "You can't divide those values!"
+    elif operation == "modulo":
+        try:
+            return num1 % num2
+        except TypeError:
+            return "You can't calculate the modulo of those values!"
+    elif operation == "power":
+        try:
+            return num1 ** num2
+        except TypeError:
+            return "You can't power those values!"
+    
