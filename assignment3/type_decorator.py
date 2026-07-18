a6422301ca6dccd962d44af8ceea0560f9dbcef3
belunatic <abelluvyou@yahoo.com>
2026-07-18 12:47:40 -0700

    task-2 decorator with arguments

---
+++ assignment3/type_decorator.py
@@ -0,0 +1,28 @@
+def type_decorator(type_of_output):
+    def decorator(func):
+        def wrapper(*args,**kwargs):
+            x = func(*args, **kwargs)
+            if(type_of_output == 'str'):
+                return str(x)
+            elif(type_of_output == 'int'):
+                return int(x)
+            elif(type_of_output == 'float'):
+                    return float(x)
+        return wrapper
+    return decorator
+
+@type_decorator('str')
+def return_int():
+    return 5
+
+@type_decorator('int')
+def return_string():
+    return 'not a number'
+
+y = return_int()
+print(type(y).__name__) # This should print "str"
+try:
+   y = return_string()
+   print("shouldn't get here!")
+except ValueError:
+   print("can't convert that string to an integer!") # This is what should happen
