72a3a3d8f706248e0cc4a68be5f9d10eb0ea141b
belunatic <abelluvyou@yahoo.com>
2026-09-07 21:25:38 -0700

    insert new order and print out the line items order - task 3 completed

--- assignment10/advanced_sql.py
+++ assignment10/advanced_sql.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 conn = sqlite3.connect('../db/lesson.db')
+conn.execute("PRAGMA foreign_keys = 1")
 cursor = conn.cursor()
 
 query = ''' 
@@ -40,6 +41,52 @@ print(cursor.fetchall())
 
 print("--------------------------------------------------")
 print('Task 2 completed')
+
+#inserting a new order
+
+cursor.execute("""INSERT INTO orders (customer_id, employee_id) 
+VALUES (
+(SELECT customer_id 
+FROM customers 
+WHERE customer_name = 'Perez and Sons'),
+(SELECT employee_id 
+FROM employees 
+WHERE first_name = 'Miranda' AND last_name = 'Harris')
+) 
+RETURNING order_id""")
+#fetch the newly inserted order_id
+new_order_id = cursor.fetchone()[0]
+# print(f"New order inserted with ID: {new_order_id}")
+
+#get the least expensive products for the order
+cursor.execute("""SELECT product_id, price 
+FROM products 
+ORDER BY price 
+LIMIT 5""")
+#create a list of product_ids to be added to the order
+products_id = [row[0] for row in cursor.fetchall()]
+# print(f"Products to be added to the order: {products_id}")
+
+#insert line items for the new order
+for product_id in products_id:
+    cursor.execute("""INSERT INTO line_items (order_id, product_id, quantity) 
+    VALUES (?, ?, ?)""", 
+    (new_order_id, product_id, 5))   
+
+#print the new order details
+cursor.execute("""SELECT li.line_item_id, li.quantity,p.product_name 
+FROM line_items AS li 
+JOIN products AS p ON li.product_id = p.product_id 
+WHERE li.order_id = ?""", 
+(new_order_id,))
+
+#print the line items for the new order
+for row in cursor.fetchall():
+    print(f"Line Item ID: {row[0]}, Quantity: {row[1]}, Product Name: {row[2]}")
+
+print("--------------------------------------------------")
+print('Task 3 completed')
+
 
 
 conn.close()
